0ba541b9098601690f72037cb4efb076d673ac6c
Mosharrof Hossain <mosharrofhossainpdl@gmail.com>
2025-09-25 21:48:34 +0600

    Banner bug fix

--- src/component/Banner/Banner.jsx
+++ src/component/Banner/Banner.jsx
@@ -1,21 +1,20 @@
 // Banner.jsx
-import React from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay, Pagination, Navigation } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 const Banner = () => {
   const images = [
-    '/src/assets/bannerimage.png',
-    '/src/assets/threewemen.png',
-    '/src/assets/fourwemen.png',
+    "/images/fourwemen.png",
+    "/images/threewemen.png",
+    "/images/fourwemen.png",
   ];
 
   return (
-    
-      <div className="w-full max-w-7xl mx-auto px-2 md:px-4 lg:px-6 py-4 h-[200px] sm:h-[300px] md:h-[400px] lg:h-[600px]">
+    <div className="w-full max-w-7xl mx-auto px-2 md:px-4 lg:px-6 py-4 h-[200px] sm:h-[300px] md:h-[400px] lg:h-[600px]">
       <Swiper
         spaceBetween={30}
         centeredSlides={true}
